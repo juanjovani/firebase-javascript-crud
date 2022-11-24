fd736bfbde11c5d80ee6b81f410cc189047ff21f
jovani <jovasreyes571@gmail.com>
2022-11-24 10:30:33 -0600

    agregando base de datos

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
+import {saveTask} from './firebase.js'
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+window.addEventListener("DOMContentLoaded", async (e) => {
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCsysEF2YHa0lCJJSki6JgDgmCu4Lc1FWQ",
-    authDomain: "fir-javascript-crud2003.firebaseapp.com",
-    projectId: "fir-javascript-crud2003",
-    storageBucket: "fir-javascript-crud2003.appspot.com",
-    messagingSenderId: "485027356482",
-    appId: "1:485027356482:web:d8ddfb794571fe9a18b2db"
-  };
+})
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const taskForm = document.getElementById("task-form");
 
+taskForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const title = taskForm["task-title"];
+  const description = taskForm["task-description"];
+
+  saveTask(title.value, description.value)
+
+  taskForm.reset();
+})
