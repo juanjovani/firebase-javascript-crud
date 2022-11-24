f8febbe863bdfd645855ba1909bc1262bc74fba2
jovani <jovasreyes571@gmail.com>
2022-11-24 09:38:27 -0600

    commit inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCsysEF2YHa0lCJJSki6JgDgmCu4Lc1FWQ",
+    authDomain: "fir-javascript-crud2003.firebaseapp.com",
+    projectId: "fir-javascript-crud2003",
+    storageBucket: "fir-javascript-crud2003.appspot.com",
+    messagingSenderId: "485027356482",
+    appId: "1:485027356482:web:d8ddfb794571fe9a18b2db"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
